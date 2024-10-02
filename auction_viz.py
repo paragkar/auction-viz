af7edd32195d7b006e4185586b5d867fc662e78e
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-10-02 13:01:19 +0530

    Add files via upload

--- auction_viz.py
+++ auction_viz.py
@@ -1884,9 +1884,9 @@ chart_data_flag = False #set this to true only if this chart exists.
 with st.sidebar:
 	selected_dimension = option_menu(
 		menu_title = "Select a Menu",
-		options = ["Spectrum Bands", "Auction YearWise", "Auction BandWise", "AuctionYear AllBands"], #Debug 14th June 2024
-		icons = ["1-circle-fill", "2-circle-fill", "3-circle-fill", "4-circle-fill"],
-		menu_icon = "arrow-down-circle-fill",
+		options = [ "AuctionYear AllBands"], #Debug 14th June 2024
+		# icons = ["1-circle-fill", ],
+		# menu_icon = "arrow-down-circle-fill",
 		default_index =0,
 		)
 
